973e406d628a8544a58e8f66a89e70b9d31871bb
kuchekarsonal <kuchekar.sonal217@siesgst.ac.in>
2020-04-18 22:20:26 +0530

    Water

--- app/src/main/java/com/example/getfit/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/getfit/Retrofit/RetrofitClient.java
@@ -9,12 +9,12 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit instance;
-    private static String BASE_URL = "http://192.168.43.46:3000"; //ip address of our machine
+    private static String BASE_URL = "http://192.168.43.38:3000"; //ip address of our machine
 
     public static Retrofit getInstance(){
         if(instance ==  null)
             instance = new Retrofit.Builder()
-                    .baseUrl(BASE_URL) // enter ip address, In emulatot, localhost will changed to ip address
+                    .baseUrl(BASE_URL) // enter ip address, In emulator, localhost will changed to ip address
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
 
