8eacbd535f4b3ed73c10800c5cc8d11c54e8c0e1
kuchekarsonal <kuchekar.sonal217@siesgst.ac.in>
2020-04-19 00:16:24 +0530

    count water glasses

--- app/src/main/java/com/example/getfit/Fragments/Fragment_Analysis.java
+++ app/src/main/java/com/example/getfit/Fragments/Fragment_Analysis.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.getfit.R;
 
@@ -15,6 +17,9 @@ import com.example.getfit.R;
  * A simple {@link Fragment} subclass.
  */
 public class Fragment_Analysis extends Fragment {
+    Button increment, decrement;
+    TextView  countText;
+    static  int count =0;
 
 
     public Fragment_Analysis() {
@@ -26,7 +31,28 @@ public class Fragment_Analysis extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment__analysis, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment__analysis, container, false);
+        increment = (Button)view.findViewById(R.id.increment);
+        decrement = (Button)view.findViewById(R.id.decrement);
+        countText = (TextView)view.findViewById(R.id.tvCount);
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                countText.setText(count);
+            }
+        });
+
+        decrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count --;
+                countText.setText(count);
+            }
+        });
+        return view;
+
     }
 
 }
