92f3d3b778f92f31b6931d8fbdd3a6ca798dc134
kuchekarsonal <kuchekar.sonal217@siesgst.ac.in>
2020-04-21 22:48:31 +0530

    Plan UI

---
+++ app/src/main/java/com/example/getfit/ModelClasses/DietPlanModel.java
@@ -0,0 +1,64 @@
+package com.example.getfit.ModelClasses;
+
+public class DietPlanModel {
+
+
+    DietPlanModel(){
+
+    }
+
+
+
+}
+
+
+/*
+{
+        "Breakfast": [
+        {
+        "recipe_name": "Indian Chole Aloo Tikki",
+        "servings": 2,
+        "calories": 628.0,
+        "carbs": 87.2,
+        "fats": 27.0,
+        "protein": 13.8
+        }
+        ],
+        "Lunch": [
+        {
+        "recipe_name": "Quick Whole Wheat Chapati",
+        "servings": 2,
+        "calories": 254.0,
+        "carbs": 54.4,
+        "fats": 1.4,
+        "protein": 10.2
+        },
+        {
+        "recipe_name": "Aloo Matar",
+        "servings": 1,
+        "calories": 487.0,
+        "carbs": 81.7,
+        "fats": 14.5,
+        "protein": 9.0
+        }
+        ],
+        "Dinner": [
+        {
+        "recipe_name": "Quick Whole Wheat Chapati",
+        "servings": 2,
+        "calories": 254.0,
+        "carbs": 54.4,
+        "fats": 1.4,
+        "protein": 10.2
+        }
+        ],
+        "total": {
+        "Calories": 1623.0,
+        "Carbohydrates": 277.7,
+        "Carbs percentage": 70.0,
+        "Proteins": 43.2,
+        "Proteins percent": 11.0,
+        "Fats": 44.3,
+        "Fats percent": 24.0
+        }
+        }*/
